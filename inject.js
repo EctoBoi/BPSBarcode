a1cbc52ff467883de4d0fc3fcae4dc7ec1299292
Justin <justin.banton@outlook.com>
2019-08-18 14:55:49 -0500

    Add files via upload

--- inject.js
+++ inject.js
@@ -2,6 +2,12 @@
 (function () {
 
 	window.onload = function () {
+		try{
+			inject();
+		}catch(error) {
+
+		}
+		
 		let body = document.getElementsByTagName("BODY")[0];
 		//let sku = document.getElementsByClassName("sku")[0];
 
@@ -12,27 +18,34 @@
 
 
 function inject() {
-	let sku = document.getElementsByClassName("sku")[0].innerHTML.match(/\d/g).join('')
-	let barcodeCreated = false;	
+	let regex = RegExp('\\d');
+	if(typeof document.getElementsByClassName("sku")[0] !== "undefined")
+	if(regex.test(document.getElementsByClassName("sku")[0].innerHTML)){
+		let sku = document.getElementsByClassName("sku")[0].innerHTML.match(/\d/g).join('')	
+	
+		let barcodeCreated = false;	
 
-	if (!document.getElementById('barcode')) {
-		let div = document.createElement('div');
-		let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-		svg.id = 'barcode'
-		div.appendChild(svg)
+		if (!document.getElementById('barcode')) {
+			let div = document.createElement('div');
+			let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+			svg.id = 'barcode'
+			div.appendChild(svg)
 
-		document.getElementsByClassName('top namePartPriceContainer')[1].appendChild(div);
-		barcodeCreated = true;
-	}
-	let lastSku = 'q'
-	chrome.storage.sync.get('lastSku', function (data) {
-		lastSku = data.lastSku;
-		if (sku !== lastSku || barcodeCreated) {
-			chrome.storage.sync.get('height', function (data) {
-				JsBarcode("#barcode", sku, { height: data.height });
-				chrome.storage.sync.set({ lastSku: sku });
-			});
+			document.getElementsByClassName('top namePartPriceContainer')[1].appendChild(div);
+			barcodeCreated = true;
 		}
-	})
+		let lastSku = undefined
+		chrome.storage.sync.get('lastSku', function (data) {
+			lastSku = data.lastSku;
+			if (sku !== lastSku || barcodeCreated) {
+				chrome.storage.sync.get('height', function (data) {
+					JsBarcode("#barcode", sku, { height: data.height });
+					chrome.storage.sync.set({ lastSku: sku });
+				});
+			}
+		})
+	
+	
+	}
 
 }
